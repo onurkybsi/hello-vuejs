77dc19088cf919a1add327d3b6ebd9fb7db5a928
onurkybsi <onurbpm@outlook.com>
2020-07-04 16:40:33 +0300

    Template Syntax reviewed

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-runAfterCreation = () => {
-  alert("Created!");
-};
-
-let color = ["green", "red", "blue", "gray"];
-
 window.addEventListener("load", () => {
   var app = new Vue({
     el: "#app",
     data: {
-      num: 0,
-      background: color[0],
+      data1: "message",
+      data2: "message2",
+      isButtonDisabled: true,
+      num: 4,
+      seen: true,
+      url: "https://github.com/onurkybsi",
+      event: "click",
     },
     methods: {
-      increase: function () {
-        this.num = this.num + 1;
+      doSomething: function () {
+        alert("Hi Vue.js!");
       },
     },
-    // The created hook can be used to run code after an instance is created (lifecycle hooks)
-    created: runAfterCreation,
-  });
-
-  let i = 0;
-
-  // Vue instances expose a number of useful instance properties and methods.
-  // These are prefixed with $ to differentiate them from user-defined properties.
-  app.$watch("num", function (newValue, oldValue) {
-    i++;
-    this.background = color[i % 4];
-    // This callback will be called when `app.num` changes
   });
 });
