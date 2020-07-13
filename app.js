fceb93b2234c3a207be850899b0ab7c801d309e3
onurkybsi <onurbpm@outlook.com>
2020-07-14 00:54:18 +0300

    Conditional rendering reviewed

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ window.addEventListener("load", () => {
   var app = new Vue({
     el: "#app",
     data: {
-      activeColor: "red",
-      fontSize: 30,
-      styleData: {
-        backgroundColor: "green",
-        fontSize: 50,
-      },
-      borderStyle: "solid",
-      backgroundColor: "yellow",
+      ok: true,
+      num: 14
     },
   });
 });
