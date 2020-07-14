fe98f1c68ff1446f82e95dcc20a3c2be77d0d42b
onurkybsi <onurbpm@outlook.com>
2020-07-15 01:42:22 +0300

    List rendering reviewed

--- app.js
+++ app.js
@@ -2,8 +2,20 @@ window.addEventListener("load", () => {
   var app = new Vue({
     el: "#app",
     data: {
-      ok: true,
-      num: 14
+      array: [{ message: "item1" }, { message: "item2" }],
+      iterableObject: {
+        prop1: "value1",
+        prop2: "value2",
+        prop3: "value3",
+      },
+      numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    },
+    computed: {
+      evenNumbers: function () {
+        return this.numbers.filter(function (number) {
+          return number % 2 === 0;
+        });
+      },
     },
   });
 });
