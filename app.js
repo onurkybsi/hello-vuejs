3bffa76899347bf56793a978699dcc064efaf885
onurkybsi <onurbpm@outlook.com>
2020-07-16 19:36:35 +0300

    Form input bindings reviewed

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
 window.addEventListener("load", () => {
   var app = new Vue({
     el: "#app",
-    data: {},
-    methods: {
-      say: function (message) {
-        alert(message);
-      },
-      warn: function (message, event) {
-        if (event) {
-          event.preventDefault();
-        }
-        alert(message);
-      },
-      doThis: function () {
-        alert("Event propagation stopped");
-      },
-      enter: function () {
-        alert("You pressed enter");
-      },
-      ctrlClick: function () {
-        alert("You pressed CTRL and then click");
-      },
+    data: {
+      message: "",
+      multiLineMessage: "",
+      checked: false,
+      picked: "",
+      selected: "",
+      locked: "",
+      toggle: "no",
+      age: "",
+      message: "",
     },
+    methods: {},
   });
 });
